0c2a76ce9593c743be20e3a24a945c029e76856b
aiisell00 <a.mdoffa08@gmail.com>
2024-11-01 13:46:29 +0400

    new task

---
+++ js task/script.js
@@ -0,0 +1,33 @@
+
+// Array.prototype.num = ()  =>  {
+//     for (let i = 0; i < this.length; i++) {
+//         this[i] = this[i] * 2;
+//         console.log(num1);
+//     }
+// };
+// const num1 = [1,2,3,4,5];
+// num1.num();
+
+// Array.prototype.num = function() {
+//     for (let i = 0; i < this.length; i++) {
+      
+//         console.log( "metod inited" + " " + i + "~"+ this[i] + " " + "metod finished");
+//     }
+// };
+// const num1 = ["S","L","V"];
+// num1.num();
+
+
+// let test = num1.map(x => x+2)
+
+
+String.prototype.newWord = function(){
+
+console.log(this.split(" ").toUpperCase()+this.slice(1));
+
+}
+let word = "aysu lale" 
+
+
+// word.newWord();
+
