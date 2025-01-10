2d3d1fe5dc4b1d2ff418166b819ff46e9f185dcb
aiisell00 <a.mdoffa08@gmail.com>
2025-01-10 13:40:20 +0400

    css

---
+++ css-1/src/components/HeaderFoto/HeaderFoto.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styles from "./HeaderFoto.module.scss";
+
+const HeaderFoto = () => {
+  return (
+    <div className={styles.slide}>
+      <div className={styles.div}>
+        <h1>Contextua Images</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Sed odio
+          nibh, tincidunt adipiscing, pretium nec, tincidunt id, enim. Fusce
+          scelerisque nunc vitae nisl. Quisque quis urna in velit dictum
+          pellentesque.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default HeaderFoto;
---
+++ css-1/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,35 @@
+// src/components/MyComponent.js
+import React from 'react';
+import styles from './Navbar.module.scss';
+
+function Navbar() {
+  return (
+    <>
+
+    <div className={styles.navTop}>
+      <h1>Travelify</h1>
+      <p>The best FREE travel blog WordPress theme</p>
+    </div>
+    <div className={styles.navbar}>
+      <ul>
+        <li><a href="">Home</a></li>
+        <li><a href="">Download Now!</a></li>
+        <li><a href="">Blog</a></li>
+        <li><a href="">New York</a></li>
+        <li><a href="">New York</a></li>
+        <li><a href="">Other</a></li>
+        <li><a href="">Other</a></li>
+        <li><a href="">WooCommerce</a></li>
+        <li><a href="">Contact us</a></li>
+      </ul>
+
+
+    </div>
+    <div className={styles.slide}>
+    </div>
+
+    </>
+  );
+}
+
+export default Navbar;
